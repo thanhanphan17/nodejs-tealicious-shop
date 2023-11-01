63653a471d95cb30c2a0ab9154d7d7e2f747ad2c
thanhanphan17 <thanhanphan17@gmail.com>
2023-11-02 04:22:36 +0700

    update cmd using dotenv run

--- Makefile
+++ Makefile
@@ -1,15 +1,15 @@
 # make run env=local|prod
-run:
-	@NODE_ENV=${env} npm run dev
+run: 
+	@dotenv -f env/${env}.env run npm run dev
 
 # make db_push env=local|prod
 db_push:
-	dotenv -f ./env/${env}.env run npx prisma db push
+	@dotenv -f ./env/${env}.env run npx prisma db push
 
 # make db_migrate env=local|prod
 db_migrate:
-	dotenv -f ./env/${env}.env run npx prisma migrate dev --name init
+	@dotenv -f ./env/${env}.env run npx prisma migrate dev --name init
 
 # make prisma_studio env=local|prod
 prisma_studio:
-	dotenv -f ./env/${env}.env run npx prisma studio
+	@dotenv -f ./env/${env}.env run npx prisma studio
--- src/configs/config.database.ts
+++ src/configs/config.database.ts
@@ -1,11 +1,9 @@
-import '~/configs/config.init'
-
 const MongoConfig = {
-  host: (process.env.DB_HOST as string) || 'localhost',
-  port: (process.env.DB_PORT as string) || '27017',
-  user: (process.env.DB_USER as string) || 'root',
-  dbname: (process.env.DB_NAME as string) || 'tealicious_db',
-  password: (process.env.DB_PASSWORD as string) || '0000'
+  host: (process.env.MG_DB_HOST as string) || 'localhost',
+  port: (process.env.MG_DB_PORT as string) || '27017',
+  user: (process.env.MG_DB_USER as string) || 'root',
+  dbname: (process.env.MG_DB_NAME as string) || 'tealicious_db',
+  password: (process.env.MG_DB_PASSWORD as string) || '0000'
 }
 
 export default MongoConfig
