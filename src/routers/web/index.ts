c30f1c3ddc9e493697082635933eb46b0ba036ac
thanhanphan17 <thanhanphan17@gmail.com>
2023-11-11 14:00:03 +0700

    format code, fix route

--- src/routers/web/index.ts
+++ src/routers/web/index.ts
@@ -1,14 +1,8 @@
 import express from 'express'
-import healthCheckRouter from '~/routers/web/health-check'
-import home from '~/routers/web/shop/index'
-import aboutus from '~/routers/web/shop/aboutus'
-import index from '~/routers/web/shop/index'
+import shop from '~/routers/web/shop/index'
 
 const router = express.Router()
 
-
-router.use('/', home) 
-
-router.use('/aboutus', aboutus) 
+router.use('/', shop)
 
 export default router
