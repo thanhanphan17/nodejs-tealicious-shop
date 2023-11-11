4fcbd3bf52efdeb53c8348620de7a5ac45eb8a26
edwardpham94 <phamhonggiabaoqn0904@gmail.com>
2023-11-11 14:17:54 +0700

    add router

--- src/routers/web/shop/index.ts
+++ src/routers/web/shop/index.ts
@@ -10,4 +10,40 @@ router.get('/about-us', function (req, res, next) {
     res.render('shop/about-us')
 })
 
+router.get('/cart', function (req, res, next) {
+    res.render('shop/cart')
+})
+
+router.get('/checkout', function (req, res, next) {
+    res.render('shop/checkout')
+})
+
+router.get('/contact', function (req, res, next) {
+    res.render('shop/contact')
+})
+
+router.get('/forgotpassword', function (req, res, next) {
+    res.render('shop/forgotpassword')
+})
+
+router.get('/login', function (req, res, next) {
+    res.render('shop/login')
+})
+
+router.get('/pages', function (req, res, next) {
+    res.render('shop/pages')
+})
+
+router.get('/sign-up', function (req, res, next) {
+    res.render('shop/signup')
+})
+
+router.get('/about-us', function (req, res, next) {
+    res.render('shop/about-us')
+})
+
+router.get('/thankyou', function (req, res, next) {
+    res.render('shop/thankyou')
+})
+
 export default router
