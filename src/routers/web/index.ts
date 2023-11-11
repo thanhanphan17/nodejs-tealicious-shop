208ef89082648754bc57c0727db6f527613a33f1
edwardpham94 <phamhonggiabaoqn0904@gmail.com>
2023-11-11 11:10:18 +0700

    update home and aboutus

--- src/routers/web/index.ts
+++ src/routers/web/index.ts
@@ -1,8 +1,14 @@
 import express from 'express'
 import healthCheckRouter from '~/routers/web/health-check'
+import home from '~/routers/web/shop/index'
+import aboutus from '~/routers/web/shop/aboutus'
+import index from '~/routers/web/shop/index'
 
 const router = express.Router()
 
-router.use('/', healthCheckRouter)
+
+router.use('/', home) 
+
+router.use('/aboutus', aboutus) 
 
 export default router
