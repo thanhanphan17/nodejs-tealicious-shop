14de770fbf3405402254a43cd52786952ec82262
edwardpham94 <phamhonggiabaoqn0904@gmail.com>
2023-11-11 14:07:47 +0700

    update route

--- src/routers/web/index.ts
+++ src/routers/web/index.ts
@@ -1,14 +1,20 @@
 import express from 'express'
 import healthCheckRouter from '~/routers/web/health-check'
 import home from '~/routers/web/shop/index'
-import aboutus from '~/routers/web/shop/aboutus'
-import index from '~/routers/web/shop/index'
+
 
 const router = express.Router()
 
 
 router.use('/', home) 
-
 router.use('/aboutus', aboutus) 
+router.use('/cart', cart) 
+router.use('/checkout', checkout) 
+router.use('/contact', contact) 
+router.use('/login', login) 
+router.use('/signup', signup) 
+router.use('/forgotpassword', forgotpassword) 
+router.use('/pages', pages) 
+router.use('/thankyou', thankyou) 
 
 export default router
--- src/routers/web/shop/index.ts
+++ src/routers/web/shop/index.ts
@@ -1,11 +1,13 @@
 import express from 'express'
-import AppConfig from '~/configs/config.app'
 
 const router = express.Router()
 
-router.get('/', function(req, res, next) {
-    res.render('shop/index');
-  });
+router.get('/', function (req, res, next) {
+    res.render('shop/index')
+})
 
+router.get('/about-us', function (req, res, next) {
+    res.render('shop/about-us')
+})
 
 export default router
