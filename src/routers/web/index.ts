7cfaf9908ff0da5ce38eef8034225a6b4b3418d8
thanhanphan17 <thanhanphan17@gmail.com>
2023-11-13 17:51:58 +0700

    css fix path router admin

--- src/routers/web/index.ts
+++ src/routers/web/index.ts
@@ -1,8 +1,10 @@
 import express from 'express'
 import shop from '~/routers/web/shop/index'
+import admin from '~/routers/web/admin/index'
 
 const router = express.Router()
 
 router.use('/', shop)
+router.use('/admin', admin)
 
 export default router
