94f96699816ee4d0b481f06ddc08fb32bcf2c629
edwardpham94 <phamhonggiabaoqn0904@gmail.com>
2023-11-13 13:54:56 +0700

    update admin route

---
+++ src/routers/web/admin/index.ts
@@ -0,0 +1,41 @@
+import express from 'express'
+
+const router = express.Router()
+
+router.get('/dashboard', function (req, res, next) {
+    res.render('admin/index')
+})
+
+router.get('/user', function (req, res, next) {
+    res.render('admin/user')
+})
+
+router.get('/product', function (req, res, next) {
+    res.render('admin/product')
+})
+
+router.get('/order', function (req, res, next) {
+    res.render('admin/order')
+})
+
+router.get('/order', function (req, res, next) {
+    res.render('admin/order')
+})
+
+router.get('/login-admin', function (req, res, next) {
+    res.render('admin/login')
+})
+
+
+router.get('/add-product', function (req, res, next) {
+    res.render('admin/add-product')
+})
+
+
+router.get('/add-order', function (req, res, next) {
+    res.render('admin/add-order')
+})
+
+
+
+export default router
