071fc6e2de4a5bcdab602e411799db48f747a470
thanhanphan17 <thanhanphan17@gmail.com>
2023-12-05 10:36:23 +0700

    fix auth api route

--- src/routers/api/auth.ts
+++ src/routers/api/auth.ts
@@ -9,6 +9,7 @@ router.post('/register', authController.register)
 
 router.get('/logout', loginRequired, authController.logout)
 router.get('/refresh-token', loginRequired, authController.refreshToken)
-router.get('/verify-token', adminRequired, authController.verifyToken)
+router.get('/verify-admin-token', adminRequired, authController.verifyToken)
+router.get('/verify-user-token', loginRequired, authController.verifyToken)
 
 export default router
