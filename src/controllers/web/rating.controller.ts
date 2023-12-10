8a08d2414b12c17fad84884d8746396f414e283c
edwardpham94 <phamhonggiabaoqn0904@gmail.com>
2023-12-10 16:15:40 +0700

    update review

--- src/controllers/web/rating.controller.ts
+++ src/controllers/web/rating.controller.ts
@@ -5,7 +5,6 @@ import appConfig from '~/configs/config.app'
 
 class ProductRatingController {
     createRating = catchAsync(async (req: any, res: any, next: NextFunction) => {
-        console.log('startttttttttttttttttttttttttttttttttttttttttttttt')
         const { productId, rating, comment } = req.body
         const accessToken = req.cookies.accessToken
         const refreshToken = req.cookies.refreshToken
@@ -36,17 +35,12 @@ class ProductRatingController {
     listRatings = catchAsync(async (req: any, res: any, next: NextFunction) => {
         console.log('rating listtttttttttttttttttttt')
         const page = 0
-        const limit = 4
+        const limit = 10
 
         const url = `${appConfig.apiURL}/api/rating/list?page=${page}&limit=${limit}&productId=${req.query.id}`
         console.log('url comtroller  ' + url)
-        // const url =
-        //     'https://yentraquan.shop/api/rating/list?page=0&limit=4&productId=d11627a3-d75e-4e65-b55e-94e24a3efaec'
         try {
             const response = await axios.get(url)
-            // console.log('ratingg controller ' + response.data.data.ratings[0].id)
-
-            // console.log('code', response.data.status)
             if (response.data.status == 200) {
                 return response.data.data.ratings
             }
